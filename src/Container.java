19878f21024e78d4bd1dee2864111f658733702b
Alexandr Menyaylov <new.mnlv1@gmail.com>
2022-01-06 19:09:11 +0300

    Third message

--- src/Container.java
+++ src/Container.java
@@ -5,7 +5,7 @@ public class Container {
         int n0 = 1;
         int n1 = 1;
         int n2;
-        String name = "Alex";
+        String name;
         for (int i = 3; i < 11; i++) {
             n2 = n0+n1;
             System.out.print(n2 + " ");
