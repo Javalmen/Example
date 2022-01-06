18a986e8c69c36648dcd129b7ff2a0ff9cf50b91
Alexandr Menyaylov <new.mnlv1@gmail.com>
2022-01-06 18:56:52 +0300

    message1

---
+++ src/Container.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Container {
+    public static void main(String[] args) {
+        int n0 = 1;
+        int n1 = 1;
+        int n2;
+        for (int i = 3; i < 11; i++) {
+            n2 = n0+n1;
+            System.out.print(n2 + " ");
+            n0 = n1;
+            n1 = n2;
+        }
+    }
+}
+
+
